f34bceb43500ad33adc52d3ee52c6ca1618aeff7
Rahul Unniraj <rahulunniraj@Rahuls-MacBook-Air.local>
2024-11-16 22:10:56 +0530

    frontend link commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port =3000
 connectDB();
 
 app.use(cors({
-  origin:["http://localhost:5173","https://final-project-frontend-xdxk-pt8u3ld17-jeenas-projects.vercel.app"],
+  origin:["http://localhost:5173","https://final-project-frontend-8gta.vercel.app"],
   credentials:true,
   methods:["GET","POST","PUT","DELETE","OPTIONS"]
 
